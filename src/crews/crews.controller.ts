1a3f296ed8916c11cbed9b981fa019f3b684fc72
akmaliatrias08 <akmaliatrias08@gmail.com>
2024-11-21 10:43:41 +0700

    fix: add authorization in crew

--- src/crews/crews.controller.ts
+++ src/crews/crews.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { CrewsService } from './crews.service';
 import { CreateCrewDTO } from './dto/create-crew.dto';
 import { UpdateCrewDTO } from './dto/update-crew.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('crews')
 export class CrewsController {
@@ -10,27 +11,32 @@ export class CrewsController {
     ){}
 
     
+    @UseGuards(JwtAuthGuard)
     @Get()
     getAll(){
         return this.crewsService.getAll()
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     getById(@Param('id', ParseUUIDPipe) id: string){
         return this.crewsService.getById(id)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     create(@Body() createCrewDTO: CreateCrewDTO){
         return this.crewsService.create(createCrewDTO)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put('/:id')
     update(@Param('id', ParseUUIDPipe) id: string, @Body() updateCrewDTO: UpdateCrewDTO){
         console.log(updateCrewDTO, "halow")
         return this.crewsService.updateById(id, updateCrewDTO)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete('/:id')
     deleteById(@Param('id', ParseUUIDPipe) id: string){
         return this.crewsService.deleteById(id)
